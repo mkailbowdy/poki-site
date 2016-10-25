ca99f489545c0450d4e3ec2de21df3d83dcbd562
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2016-10-24 18:05:16 -0700

    added all files

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* collapse header after item clicked */
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Hide Header on on scroll down
+$(".navbar-fixed-top").autoHidingNavbar({
+  // see next for specifications
+});
